021149eb8f431352123f0fa22933d5a918b5af6a
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-17 21:37:11 +0100

    Create class StatisticsHelper with sum calculating

---
+++ zad1/src/StatisticsHelper.java
@@ -0,0 +1,9 @@
+public class StatisticsHelper {
+    public int calculateSum(int[] numbers) {
+        int sum = 0;
+        for (int num: numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+}
