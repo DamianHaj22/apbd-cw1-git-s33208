8c93f4d56133f2c0f35441b67d82c81158e1f15c
Damian Hajduk <s33208@pjwstk.edu.pl>
2026-03-17 21:52:20 +0100

    Dodanie wyskakiwania bledu w Main i obliczanie z wyśweitlaniem w Main

--- zad1/src/Main.java
+++ zad1/src/Main.java
@@ -2,9 +2,16 @@ public class Main {
     public static void main(String[] args) {
         int[] data = {1, 2, 3, 4};
 
+        if (data == null || data.length == 0) {
+            System.err.println("Brak danych");
+            return;
+        }
+
         StatisticsHelper helper = new StatisticsHelper();
         int totalSum = helper.calculateSum(data);
+        double average = helper.calculateAverage(data);
 
-        System.out.println("Sum is: " + totalSum);
+        System.out.println("Total sum is: " + totalSum);
+        System.out.println("Average is: " + average);
     }
 }
